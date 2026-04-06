e93c90789ecded6c3f3dd4472691e2a06a945526
achiwa.kosuke <achiwa.kosuke@itfllc.co.jp>
2026-04-06 17:23:56 +0900

    Initial commit - Antigravity Video Editor Pro

---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    title: "Antigravity Video Editor",
+    backgroundColor: '#121212',
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js') // We'll add a simple preload
+    }
+  });
+
+  win.loadFile('index.html');
+  
+  // Custom Menu Hide or standard
+  // Menu.setApplicationMenu(null); 
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
